8cee5147c0daeebb50498196d8d7a01db18ed435
ryota-murakami <dojce1048@gmail.com>
2016-03-26 19:37:55 +0900

    config.fish > set path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,9 @@
 alias vi="/usr/local/bin/vim"
+
+set -U fish_user_paths $fish_user_paths /usr/local/bin
+set -U fish_user_paths $fish_user_paths $HOME/.rbenv/versions/2.2.3/bin
+set -U fish_user_paths $fish_user_paths $HOME/node_modules/.bin
+set -U fish_user_paths $fish_user_paths $HOME/.composer/vendor/bin
+
+# lsコマンドのカラー
+set -U LSCOLORS gxfxcxdxbxegedabagacad
