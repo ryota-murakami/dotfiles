0a63310bf7adfb0acbcaeaaa8cdfd6b47f29ec2c
ryota-murakami <dojce1048@gmail.com>
2016-04-01 23:45:15 +0900

    config.fish > import func from .bash_profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,17 @@ set -U fish_user_paths $fish_user_paths $HOME/.composer/vendor/bin
 
 # lsコマンドのカラー
 set -U LSCOLORS gxfxcxdxbxegedabagacad
+
+# Brewfile controller
+function brewfile
+  [[ ! -r ~/dotfiles/Brewfile ]] and echo "Brewfile not found(◎-◎；)" and return 1
+
+  [[ $1 = "update" ]] and brew bundle dump --force --file=~/dotfiles/Brewfile and echo "Brewfile has been updated." and return 0
+end
+
+
+# .editorconfig生成
+function geneditconf
+  cp ~/dotfiles/.editorconfig .
+  ll .editorconfig
+end
