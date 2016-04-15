f893bb6ad5687904d8ae985a0f2ae428e3c74a2e
ryota-murakami <dojce1048@gmail.com>
2016-04-15 14:53:07 +0900

    [config.fish] add phpbrew config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,17 @@ set -U fish_user_paths $fish_user_paths $HOME/.composer/vendor/bin
 # lsコマンドのカラー
 set -U LSCOLORS gxfxcxdxbxegedabagacad
 
-# Brewfile controller
-function brewfile
-  [[ ! -r ~/dotfiles/Brewfile ]] and echo "Brewfile not found(◎-◎；)" and return 1
-
-  [[ $1 = "update" ]] and brew bundle dump --force --file=~/dotfiles/Brewfile and echo "Brewfile has been updated." and return 0
+# fish用のphpbrew初期化ファイル
+if [ -f ~/.phpbrew/phpbrew.fish ]
+  source ~/.phpbrew/phpbrew.fish
 end
 
+# Brewfile controller
+function brewfile
+  [ ! -r ~/dotfiles/Brewfile ] and echo "Brewfile not found(◎-◎；)" and return 1
+
+  [ $1 = "update" ] and brew bundle dump --force --file=~/dotfiles/Brewfile and echo "Brewfile has been updated." and return 0
+end
 
 # .editorconfig生成
 function geneditconf
