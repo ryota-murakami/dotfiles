e33f9598dc40970bb8a9d4fcc5459d4557cd8afa
ryota-murakami <dojce1048@gmail.com>
2017-06-28 12:35:43 +0900

    ssdinfo:green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,5 +36,9 @@ function cpuinfo
     system_profiler SPHardwareDataType
 end
 
+function ssdinfo
+    du -hs * | gsort -hr
+end
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/ryota.murakami/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/ryota.murakami/google-cloud-sdk/path.fish.inc'; else; . '/Users/ryota.murakami/google-cloud-sdk/path.fish.inc'; end; end
