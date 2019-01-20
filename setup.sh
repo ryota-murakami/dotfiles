a93451e44a62ed46483624c74c6e971d75275dfb
ryota-murakami <dojce1048@gmail.com>
2019-01-20 23:19:08 +0900

    comment out:green_apple:

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
 # Key Repeat
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
+#defaults write -g InitialKeyRepeat -int 10
+#defaults write -g KeyRepeat -int 1
 
 #=========================================================
 # Create Symlink
