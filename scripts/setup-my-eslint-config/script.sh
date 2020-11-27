8117847ad11ffd92febca5618f7e45a09da941b9
ryota-murakami <dojce1048@gmail.com>
2020-11-27 14:28:00 +0900

    [scripts/setup-my-eslint-config] fix typo :green_apple:

--- scripts/setup-my-eslint-config/script.sh
+++ scripts/setup-my-eslint-config/script.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-yarn add -D eslint-config-prettier eslint-config-typescript eslint-plugin-prettier prettier eslint-config-react-app @typescript-eslint/eslint-plugin @typescript-eslint/parser babel-eslint eslint eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react@ eslint-plugin-react-hooks@
+yarn add -D eslint-config-prettier eslint-config-typescript eslint-plugin-prettier prettier eslint-config-react-app @typescript-eslint/eslint-plugin @typescript-eslint/parser babel-eslint eslint eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
 
 cp $SCRIPT_DIR/.eslintrc .eslintrc
 cp $SCRIPT_DIR/.eslintignore .eslintignore
