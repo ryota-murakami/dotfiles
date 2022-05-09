aab5da2c0f8de196c3ed2d696327e201d8d17036
ryota-murakami <dojce1048@gmail.com>
2022-05-10 02:05:03 +0900

    [eslint] add sort-keys-fix/sort-keys-fix plugin :green_apple:

--- eslint/install.sh
+++ eslint/install.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-yarn add -D eslint eslint-config-prettier eslint-config-typescript eslint-plugin-prettier prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react-hooks eslint-plugin-cypress eslint-import-resolver-typescript
+yarn add -D eslint eslint-config-prettier eslint-config-typescript eslint-plugin-prettier prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react-hooks eslint-import-resolver-typescript eslint-plugin-sort-keys-fix
 
 cp $SCRIPT_DIR/.eslintrc .eslintrc
 cp $SCRIPT_DIR/.eslintignore .eslintignore
