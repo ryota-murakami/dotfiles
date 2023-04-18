6f0878667b3374b078a92a61759f9ded648cab5a
ryota-murakami <dojce1048@gmail.com>
2023-04-18 13:50:41 +0900

    [fish] add wiki alias :green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ alias lf="perl -pe 's/\n/\n\n/g'"
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --guest"
 alias wanip="curl https://ipinfo.io/ip"
 alias desktopjs="/Applications/Chromium.app/Contents/MacOS/Chromium --auto-open-devtools-for-tabs"
+alias wiki="code /Users/ryota.murakami/laststance/react-web-dev-wiki"
 
 set -x PATH /opt/homebrew/bin $HOME/.deno/bin $HOME/.cargo/bin $HOME/Library/Python/2.7/bin $PATH
 
