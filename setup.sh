03a96763c4d21660dca6a1b5ed2e20af77a0c092
ryota-murakami <dojce1048@gmail.com>
2023-07-22 19:12:58 +0900

    [setup.sh] remove gem install bundler :green_apple:

--- setup.sh
+++ setup.sh
@@ -9,39 +9,35 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Find
 # Key Repeat
 #defaults write -g InitialKeyRepeat -int 10
 #defaults write -g KeyRepeat -int 1
-
+# @TODO Add finished message with seelptime
 
 #=========================================================
 # Install essential tools from Brewfile
 #=========================================================
 brew bundle
+# @TODO Add finished message with seelptime
 
 #=========================================================
 # Create Symlink
 #=========================================================
+# @TODO Add what start task message with seelptime
 cd $HOME/dotfiles 
 for f in .??*
 do
     [[ "$f" =~ ^[^\.].* ]] && continue
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
-    [[ -e $HOME/$f ]] && echo "${f} is already exists." && continue
 
-    ln -s $HOME/dotfiles/$f $HOME/$f
+    ln -sf $HOME/dotfiles/$f $HOME/$f
 
     echo "${f} setup successfully."
 done
-
+# @TODO Add finish task message with seelptime
 #=========================================================
 # Setup fish-sell (fish-shell is contained in Brewfile)
 #=========================================================
 grep -E "/usr/local/bin/fish" /etc/shells || echo "/usr/local/bin/fish" | sudo tee -a /etc/shells # If there is no fish in the shell listing file, add it to the end of the line.
 chsh -s /usr/local/bin/fish
-
-#=========================================================
-# Install Bundler
-#=========================================================
-gem install bundler
 
 #=========================================================
 # Finish
