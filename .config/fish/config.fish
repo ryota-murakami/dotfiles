d7f95a610360543a23e26f750d9f528fe0379fd2
Ryota Murakami <dojce1048@gmail.com>
2024-05-04 12:27:10 +0900

    [fish] update alias :green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,7 +22,7 @@ alias fork="cd ~/fork"
 alias io="cd ~/laststance/laststance.io"
 alias react-next-tailwind-ui="cd ~/react-next-tailwind-ui"
 alias ui-library="cd ~/ui-library"
-alias corelive="cd ~/corelive"
+alias unfare="cd ~/unfare"
 
 set -x LESS "-R"
 set -x PATH $HOME/nvim-macos/bin $PATH
