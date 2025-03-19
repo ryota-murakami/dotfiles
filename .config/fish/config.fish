8aa905ea54019bbdf7779bdd0b0e4d6b29a9845b
Ryota Murakami <dojce1048@gmail.com>
2025-03-19 16:02:09 +0900

    [fish] update fish version :green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -240,3 +240,7 @@ pyenv init - | source
 
 zoxide init fish | source
 
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
