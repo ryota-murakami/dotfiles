ba43ded17298b7477c72146a7151ff7eaa916d4f
Ryota Murakami <dojce1048@gmail.com>
2025-05-08 10:40:23 +0900

    [fish] remove pmx alias from fish config :green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,6 @@ alias dotfiles="cd ~/dotfiles"
 alias nsx="cd ~/nsx"
 alias last="cd ~/laststance"
 alias io="cd ~/laststance/laststance.io"
-alias pmx="pnpm dlx"
 
 set -gx LSCOLORS "CxGxBxDxCxEgEdxbxgxcxd"
 set -gx EZA_COLORS "di=1;32"
