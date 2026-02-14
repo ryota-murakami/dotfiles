3e5c14b93f5c61e0dfe4adefefe8cde6e50231de
Ryota Murakami <dojce1048@gmail.com>
2026-02-14 11:12:15 +0900

    [gitignore] add 'sidecar' to ignore list and ensure 'cmus' is included for cleaner repository :green_apple:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,3 +24,5 @@
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+fish_add_path -gm /Users/ryotamurakami/go/bin
